0171a6cd6285c87b6d5ec6daeda8c1a71c44837f
nagyb3 <nagybence2003@gmail.com>
2022-12-27 23:24:17 +0100

    make singleRound function

--- main.js
+++ main.js
@@ -2,12 +2,31 @@ function getComputerChoice() {
     let randomnum = Math.floor(Math.random() * 3);
     switch (randomnum) {
         case 0:
-            return "Rock";
+            return "rock";
         case 1:
-            return "Paper";
+            return "paper";
         case 2:
-            return "Scissors";
+            return "scissors";
     }
 }
 
-console.log(getComputerChoice())
+function singleRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    if (playerSelection === "rock" && computerSelection === "Scissors") {
+        return "You win! Rock beats Scissors";
+    } else if (playerSelection === "paper" && computerSelection === "Scissors") {
+        return "You lose! Scissors beats Paper";
+    } else if (playerSelection === "scissors" && computerSelection === "Rock") {
+        return "You lose! Rock beats Scissors"
+    } else if (playerSelection === "paper" && computerSelection === "Rock") {
+        return "You win! Paper beats Rock"
+    } else if (playerSelection === "scissors" && computerSelection === "Paper") {
+        return "You Win! Scissors beats paper"
+    } else if (playerSelection === "rock" && computerSelection === "Paper") {
+        return "You lose! Paper beats rock"
+    } else if (playerSelection === computerSelection.toLowerCase()) {
+        return "It's a tie!"
+    }
+}
+
+console.log(singleRound("paper", "Rock"));
